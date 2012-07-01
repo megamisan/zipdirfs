acf1ff2bd482549c35a990a755187c4beb490415
Pierrick Caillon <pierrick.caillon@megami.fr>
2012-07-01 19:37:34 +0000

    Forgotten save actions.

--- include/ZipEntry.h
+++ include/ZipEntry.h
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with zipdirfs.  If not, see <http://www.gnu.org/licenses/>.
  *
- * $
+ * $Id$
  */
 #ifndef ZIPENTRY_H
 #define ZIPENTRY_H
--- src/ZipFile.cpp
+++ src/ZipFile.cpp
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with zipdirfs.  If not, see <http://www.gnu.org/licenses/>.
  *
- * $
+ * $Id$
  */
 #include "ZipFile.h"
 #include "ZipEntry.h"
